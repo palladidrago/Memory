bac4e583f2afe2a59f3e4e1354c3785929140a8d
Max Power <palladidrago@gmail.com>
2020-09-16 14:39:03 +0300

    Second commit

---
+++ Memory Thatcher/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Memory_Thatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pic2 = new System.Windows.Forms.PictureBox();
+            this.Pic3 = new System.Windows.Forms.PictureBox();
+            this.Pic4 = new System.Windows.Forms.PictureBox();
+            this.Pic5 = new System.Windows.Forms.PictureBox();
+            this.Pic6 = new System.Windows.Forms.PictureBox();
+            this.Pic8 = new System.Windows.Forms.PictureBox();
+            this.Pic7 = new System.Windows.Forms.PictureBox();
+            this.Pic1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Pic2
+            // 
+            this.Pic2.Image = global::Memory_Thatcher.Properties.Resources.Roni;
+            this.Pic2.Location = new System.Drawing.Point(318, 12);
+            this.Pic2.Name = "Pic2";
+            this.Pic2.Size = new System.Drawing.Size(300, 374);
+            this.Pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic2.TabIndex = 7;
+            this.Pic2.TabStop = false;
+            this.Pic2.Click += new System.EventHandler(this.Pic2_Click);
+            // 
+            // Pic3
+            // 
+            this.Pic3.Image = global::Memory_Thatcher.Properties.Resources.Roni;
+            this.Pic3.Location = new System.Drawing.Point(624, 12);
+            this.Pic3.Name = "Pic3";
+            this.Pic3.Size = new System.Drawing.Size(300, 374);
+            this.Pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic3.TabIndex = 6;
+            this.Pic3.TabStop = false;
+            this.Pic3.Click += new System.EventHandler(this.Pic3_Click);
+            // 
+            // Pic4
+            // 
+            this.Pic4.Image = global::Memory_Thatcher.Properties.Resources.Roni;
+            this.Pic4.Location = new System.Drawing.Point(930, 12);
+            this.Pic4.Name = "Pic4";
+            this.Pic4.Size = new System.Drawing.Size(300, 374);
+            this.Pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic4.TabIndex = 5;
+            this.Pic4.TabStop = false;
+            this.Pic4.Click += new System.EventHandler(this.Pic4_Click);
+            // 
+            // Pic5
+            // 
+            this.Pic5.Image = global::Memory_Thatcher.Properties.Resources.Roni;
+            this.Pic5.Location = new System.Drawing.Point(12, 392);
+            this.Pic5.Name = "Pic5";
+            this.Pic5.Size = new System.Drawing.Size(300, 374);
+            this.Pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic5.TabIndex = 4;
+            this.Pic5.TabStop = false;
+            this.Pic5.Click += new System.EventHandler(this.Pic5_Click);
+            // 
+            // Pic6
+            // 
+            this.Pic6.Image = global::Memory_Thatcher.Properties.Resources.Roni;
+            this.Pic6.Location = new System.Drawing.Point(318, 392);
+            this.Pic6.Name = "Pic6";
+            this.Pic6.Size = new System.Drawing.Size(300, 374);
+            this.Pic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic6.TabIndex = 3;
+            this.Pic6.TabStop = false;
+            this.Pic6.Click += new System.EventHandler(this.Pic6_Click);
+            // 
+            // Pic8
+            // 
+            this.Pic8.Image = global::Memory_Thatcher.Properties.Resources.Roni;
+            this.Pic8.Location = new System.Drawing.Point(930, 392);
+            this.Pic8.Name = "Pic8";
+            this.Pic8.Size = new System.Drawing.Size(300, 374);
+            this.Pic8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic8.TabIndex = 2;
+            this.Pic8.TabStop = false;
+            this.Pic8.Click += new System.EventHandler(this.Pic8_Click);
+            // 
+            // Pic7
+            // 
+            this.Pic7.Image = global::Memory_Thatcher.Properties.Resources.Roni;
+            this.Pic7.Location = new System.Drawing.Point(624, 392);
+            this.Pic7.Name = "Pic7";
+            this.Pic7.Size = new System.Drawing.Size(300, 374);
+            this.Pic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic7.TabIndex = 1;
+            this.Pic7.TabStop = false;
+            this.Pic7.Click += new System.EventHandler(this.Pic7_Click);
+            // 
+            // Pic1
+            // 
+            this.Pic1.Image = global::Memory_Thatcher.Properties.Resources.Roni;
+            this.Pic1.Location = new System.Drawing.Point(12, 12);
+            this.Pic1.Name = "Pic1";
+            this.Pic1.Size = new System.Drawing.Size(300, 374);
+            this.Pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic1.TabIndex = 0;
+            this.Pic1.TabStop = false;
+            this.Pic1.Click += new System.EventHandler(this.Pic1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 773);
+            this.Controls.Add(this.Pic2);
+            this.Controls.Add(this.Pic3);
+            this.Controls.Add(this.Pic4);
+            this.Controls.Add(this.Pic5);
+            this.Controls.Add(this.Pic6);
+            this.Controls.Add(this.Pic8);
+            this.Controls.Add(this.Pic7);
+            this.Controls.Add(this.Pic1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Pic1;
+        private System.Windows.Forms.PictureBox Pic7;
+        private System.Windows.Forms.PictureBox Pic8;
+        private System.Windows.Forms.PictureBox Pic6;
+        private System.Windows.Forms.PictureBox Pic5;
+        private System.Windows.Forms.PictureBox Pic4;
+        private System.Windows.Forms.PictureBox Pic3;
+        private System.Windows.Forms.PictureBox Pic2;
+    }
+}
+
---
+++ Memory Thatcher/Form1.cs
@@ -0,0 +1,96 @@
+﻿using Memory_Thatcher.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_Thatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool Is1Back = true;
+
+        private bool Is2Back = true;
+
+        private bool Is3Back = true;
+
+        private bool Is4Back = true;
+
+        private bool Is5Back = true;
+
+        private bool Is6Back = true;
+
+        private bool Is7Back = true;
+
+        private bool Is8Back = true;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Pic1_Click(object sender, EventArgs e)
+        {
+            if (Is1Back) { this.Pic1.Image = Resources.Back;   }
+            else { this.Pic1.Image = Resources.Roni; }
+            Is1Back = !Is1Back;
+        }
+        private void Pic2_Click(object sender, EventArgs e)
+        {
+            if (Is2Back) { this.Pic2.Image = Resources.Back; }
+            else { this.Pic2.Image = Resources.Roni; }
+            Is2Back = !Is2Back;
+
+        }
+        private void Pic3_Click(object sender, EventArgs e)
+        {
+            if (Is3Back) { this.Pic3.Image = Resources.Back; }
+            else { this.Pic3.Image = Resources.Roni; }
+            Is3Back = !Is3Back;
+
+        }
+        private void Pic4_Click(object sender, EventArgs e)
+        {
+            if (Is4Back) { this.Pic4.Image = Resources.Back; }
+            else { this.Pic4.Image = Resources.Roni; }
+            Is4Back = !Is4Back;
+
+        }
+        private void Pic5_Click(object sender, EventArgs e)
+        {
+            if (Is5Back) { this.Pic5.Image = Resources.Back; }
+            else { this.Pic5.Image = Resources.Roni; }
+            Is5Back = !Is5Back;
+        }
+        private void Pic6_Click(object sender, EventArgs e)
+        {
+            if (Is4Back) { this.Pic6.Image = Resources.Back; }
+            else { this.Pic6.Image = Resources.Roni; }
+            Is6Back = !Is6Back;
+
+        }
+        private void Pic7_Click(object sender, EventArgs e)
+        {
+            if (Is7Back) { this.Pic7.Image = Resources.Back; }
+            else { this.Pic7.Image = Resources.Roni; }
+            Is7Back = !Is7Back;
+
+        }
+        private void Pic8_Click(object sender, EventArgs e)
+        {
+            if (Is8Back) { this.Pic8.Image = Resources.Back; }
+            else { this.Pic8.Image = Resources.Roni; }
+            Is8Back = !Is1Back;
+
+        }
+    }
+}
